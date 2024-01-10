7384e102c5bb0799ea066b7893d24d70d8f4315a
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2024-01-10 23:38:10 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>Hi</div>
+      <div>Hi again</div>
+      <div>Im a new project</div>
+    </>
+  );
+}
+
+export default App;
