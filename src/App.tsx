a996a75f881c8aac6facca2b1db0d4a726699bcc
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2024-01-10 23:44:55 +0200

    add styled components lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
 
+import styled from 'styled-components';
+
 function App() {
+  const MyStyledDiv = styled.div`
+    display: flex;
+    background: red;
+    font: 300;
+    font-style: italic;
+  `;
+
   return (
     <>
       <div>Hi</div>
       <div>Hi again</div>
       <div>Im a new project</div>
+      <MyStyledDiv>Im a styled div</MyStyledDiv>
     </>
   );
 }
