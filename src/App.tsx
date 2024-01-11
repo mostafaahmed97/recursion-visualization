a962afafdd311738fe809b547b4af1f3e7a49be1
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2024-01-11 16:58:16 +0200

    add shadcnui & tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
+import '../app/globals.css';
 
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
 import styled from 'styled-components';
 
 function App() {
@@ -9,23 +12,35 @@ function App() {
     height: 100vh;
     gap: 1rem;
     padding: 1rem;
+    background-color: rgb(236, 236, 236);
   `;
 
   const OptionsPane = styled.div`
     flex: 1;
+    padding: 1rem;
     border-radius: 6px;
-    background-color: green;
+    background-color: #fafafa;
   `;
 
   const VisualizationPane = styled.div`
     flex: 3;
+    padding: 1rem;
     border-radius: 6px;
-    background-color: blue;
+    background-color: #fafafa;
   `;
 
   return (
     <Wrapper>
-      <OptionsPane></OptionsPane>
+      <OptionsPane>
+        <h1 className="mb-2 font-bold">Recursion Visualizer</h1>
+        <Input
+          className="mb-2"
+          placeholder="Number"
+          aria-label="Number"
+        ></Input>
+        <Button style={{ marginRight: '2rem' }}>Hi</Button>
+        <Button variant={'outline'}>Hiiii</Button>
+      </OptionsPane>
       <VisualizationPane></VisualizationPane>
     </Wrapper>
   );
