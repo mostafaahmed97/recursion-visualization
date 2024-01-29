08efccdfc3d5baaad2051fa15e2439286c57c0d2
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2024-01-30 00:20:36 +0200

    add pan & zoom to generated diagram

--- src/components/VisualizationPanel.tsx
+++ src/components/VisualizationPanel.tsx
@@ -1,3 +1,5 @@
+import * as d3 from 'd3';
+
 import { useContext, useEffect } from 'react';
 
 import { VisualizationContext } from './VisualizationProvider';
@@ -19,17 +21,52 @@ export default function VisualizationPanel() {
     padding: 1rem;
     border-radius: 6px;
     background-color: #fafafa;
-    overflow: scroll;
+    overflow: hidden;
   `;
   const vizCxt = useContext(VisualizationContext);
 
   useEffect(() => {
     mermaid.contentLoaded();
+
+    (async () => {
+      console.log('hi');
+
+      const { svg } = await mermaid.render(
+        'mermaid-div',
+        vizCxt.generatedDiagram
+      );
+
+      d3.select('.mermaid-div').html(svg);
+
+      const svgEl = d3.select('svg');
+
+      svgEl
+        .style('width', '100%')
+        .style('height', '100%')
+        .style('max-width', '')
+        .html('<g>' + svgEl.html() + '</g>');
+
+      const inner = svgEl.select('g');
+      const zoom = d3.zoom().on('zoom', function (event) {
+        inner.attr('transform', event.transform);
+      });
+
+      svgEl.call(zoom);
+    })();
   });
 
   return (
     <VisualizationPanel>
-      <pre className="mermaid">{vizCxt.generatedDiagram}</pre>
+      {/* <pre className="mermaid">{vizCxt.generatedDiagram}</pre> */}
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+        className="mermaid-div"
+      >
+        a
+      </div>
     </VisualizationPanel>
   );
 }
